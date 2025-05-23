b14d6c4feefff543e58ceac378aacc99862af44d
Dmytro Barylo <bdpochta@gmail.com>
2025-05-23 17:37:17 +0200

    add generateStaticParams

--- frontend/app/issue/[slug]/page.tsx
+++ frontend/app/issue/[slug]/page.tsx
@@ -2,6 +2,16 @@ import { getAllIssueFileNames, getIssue } from "@shared/services";
 import { IssuePage } from "@components/IssuePage";
 import { redirect } from "next/navigation";
 
+export async function generateStaticParams() {
+  const files = await getAllIssueFileNames();
+
+  return (
+    files?.map((_, i) => ({
+      slug: i + 1,
+    })) ?? []
+  );
+}
+
 type Params = Promise<{ slug: string }>;
 
 export default async function Issue({ params }: { params: Params }) {
