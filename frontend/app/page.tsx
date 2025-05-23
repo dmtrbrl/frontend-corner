d5a13c1f1ea6a63dbbe4ea2b2b9f676505925f4e
Dmytro Barylo <bdpochta@gmail.com>
2025-05-23 17:28:53 +0200

    fix page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,9 @@
-import { getLastPublishedIssue } from "@shared/services";
+import { getAllIssueFileNames, getLastPublishedIssue } from "@shared/services";
 import { IssuePage } from "@components/IssuePage";
 
 export default async function Home() {
+  const allIssues = await getAllIssueFileNames();
   const issue = await getLastPublishedIssue();
 
-  return <IssuePage issue={issue} />;
+  return <IssuePage issue={issue} count={allIssues.length} />;
 }
