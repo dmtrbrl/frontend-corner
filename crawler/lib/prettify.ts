ffd6a506cb6dfe20c22daa7d18387dc4a943ea18
Dmytro Barylo <bdpochta@gmail.com>
2025-07-08 14:24:48 +0200

    Extend prettify helper

--- crawler/lib/prettify.ts
+++ crawler/lib/prettify.ts
@@ -2,6 +2,9 @@ export function prettify(text: string) {
   return text
     .replace("&#8211;", "–")
     .replace("&#8212;", "—")
+    .replace("&#8217;", "’")
+    .replace("&ldquo;", "“")
+    .replace("&rdquo;", "”")
     .replace(/(?<=\S)—(?=\S)/g, " – ")
     .trim();
 }
