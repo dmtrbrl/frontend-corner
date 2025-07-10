a3cf0322c5299c020030123011198ed2f27baaee
Dmytro Barylo <bdpochta@gmail.com>
2025-07-10 09:00:57 +0200

    fix generateMetadata

--- frontend/app/issue/[slug]/page.tsx
+++ frontend/app/issue/[slug]/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({
 }: {
   params: Params;
 }): Promise<Metadata> {
-  const { slug } = params;
+  const { slug } = await params;
   const issue = await getIssue(slug);
 
   if (!issue) {
